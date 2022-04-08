0a0bf81ba56389145e129da79070bee32d68d58c
Abhi jain <91718608+abhijain2003@users.noreply.github.com>
2022-04-08 11:06:18 +0530

    Update Land.jsx

--- src/LandingPage/Land.jsx
+++ src/LandingPage/Land.jsx
@@ -34,7 +34,11 @@ function Land() {
           <img alt="image" src={image} />
         </div>
       </div>
-      <div className="tutorial"></div>
+      <div className="tutorial">
+        <iframe width="560" height="315" 
+src="https://www.youtube.com/embed/fkchFU5wwM4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+  </iframe>
+        </div>
     </div>
   );
 }
